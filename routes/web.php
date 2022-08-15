0ba77fe1ac9bbae562860eeab442f33a2beb23ef
minh-phat <nguyenleminhkhang0608@gmail.com>
2022-08-15 10:10:20 +0700

    1

--- routes/web.php
+++ routes/web.php
@@ -70,7 +70,7 @@ Route::get('contact', [CustomerController::class, 'contact']);
 Route::get('customerLogin', [CustomerLoginController::class, 'login']); //run sign in page
 Route::get('customerRegister', [CustomerLoginController::class, 'registration']);
 Route::post('saveCustomer', [CustomerController::class, 'save']); //!not a page.
-Route::post('cart', [CustomerController::class, 'cart']); 
+Route::get('cart', [CustomerController::class, 'cart']); 
 
 
 //*user
