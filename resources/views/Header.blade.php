3234357f05c5e30c30b0a1b47e97f2635e451913
MinhTriet241203 <minhtriet241203@gmail.com>
2022-08-18 15:31:49 +0700

    Update Header.blade.php

--- resources/views/Header.blade.php
+++ resources/views/Header.blade.php
@@ -5,19 +5,19 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="apple-touch-icon" href="img/apple-icon.png">
-    <link rel="shortcut icon" type="image/x-icon" href="img/logoWebsite.ico">
+    <link rel="apple-touch-icon" href="../img/apple-icon.png">
+    <link rel="shortcut icon" type="image/x-icon" href="../img/logoWebsite.ico">
 
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/templatemo.css">
-    <link rel="stylesheet" href="css/custom.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/templatemo.css">
+    <link rel="stylesheet" href="../css/custom.css">
 
     <!-- Load fonts style after rendering the layout styles -->
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;200;300;400;500;700;900&display=swap">
-    <link rel="stylesheet" href="css/fontawesome.min.css">
-    <link rel="stylesheet" type="text/css" href="css/slick.min.css">
-    <link rel="stylesheet" type="text/css" href="css/slick-theme.css">
+    <link rel="stylesheet" href="../css/fontawesome.min.css">
+    <link rel="stylesheet" type="text/css" href="../css/slick.min.css">
+    <link rel="stylesheet" type="text/css" href="../css/slick-theme.css">
     <!--
     
 TemplateMo 559 Zay Shop
