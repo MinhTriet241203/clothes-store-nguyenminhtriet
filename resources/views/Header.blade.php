d68221925f31c7b8a3d8257b133de55feac9e549
MinhTriet241203 <minhtriet241203@gmail.com>
2022-08-18 15:33:54 +0700

    Update Header.blade.php

--- resources/views/Header.blade.php
+++ resources/views/Header.blade.php
@@ -5,8 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="apple-touch-icon" href="../img/apple-icon.png">
-    <link rel="shortcut icon" type="image/x-icon" href="../img/logoWebsite.ico">
+    <link rel="apple-touch-icon" href="../../img/apple-icon.png">
+    <link rel="shortcut icon" type="image/x-icon" href="../../img/logoWebsite.ico">
 
     <link rel="stylesheet" href="../css/bootstrap.min.css">
     <link rel="stylesheet" href="../css/templatemo.css">
