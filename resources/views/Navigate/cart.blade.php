ecebae23680ce37156abc68d8e42c6dff0d970d2
mrshine4k <111043449+mrshine4k@users.noreply.github.com>
2022-08-19 16:40:31 +0700

    Removed the sort by button cause i'm lazy

--- resources/views/Navigate/cart.blade.php
+++ resources/views/Navigate/cart.blade.php
@@ -12,10 +12,6 @@
 
                 <div class="d-flex justify-content-between align-items-center mb-4">
                     <h3 class="fw-normal mb-0 text-black">Shopping Cart</h3>
-                    <div>
-                        <p class="mb-0"><span class="text-muted">Sort by:</span> <a href="#!"
-                                class="text-body">price <i class="fas fa-angle-down mt-1"></i></a></p>
-                    </div>
                 </div>
                 <?php
 
@@ -61,7 +57,7 @@
                 <div class="card rounded-3 mb-4">
                     <div class="card-body p-3">
                         <a href="" class="btn btn-success" style="float: right">Purchase</a>
-                        <a href="{{ url('/') }}" class="btn btn-danger"
+                        <a href="{{ url()->previous() }}" class="btn btn-outline-danger"
                             style="float: right; margin-right: 1%">Back</a>
                     </div>
 
