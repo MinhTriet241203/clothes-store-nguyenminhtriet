c6be968a9350790b0d331121c4d81c241c27b9cb
MinhTriet241203 <minhtriet241203@gmail.com>
2022-08-21 22:27:51 +0700

    Profile page

--- resources/views/Customer/profile.blade.php
+++ resources/views/Customer/profile.blade.php
@@ -4,31 +4,104 @@
     <title>Contact</title>
 </head>
 
-<!-- Start Content Page -->
-<div class="container-fluid bg-light py-5">
-    <div class="col-md-6 m-auto text-center" style="display: inline-block">
-        <div class="col-md-5">
-            <div class="card rounded-3">
-                <img src="img/ProfilePics/PlaceholderPFP.jpg" alt="" height="250" width="250"
-                    style="border-radius: 25%; align-self:center">
-                <h1>{{ $data->Customer_Name }}</h1>
-                <p><i class="far fa-user-circle"></i> Username: {{ $data->Customer_Username }}</p>
-                <p><i class="fas fa-address-card"></i> Name: {{ $data->Customer_Name }}</p>
-                <p><i class="fas fa-envelope"></i> Email: {{ $data->Email }}</p>
-                <p><i class="fas fa-phone"></i> Phone: {{ $data->Phone }}</p>
-                <p><i class="fas fa-home"></i> Address: {{ $data->Address }}</p>
-                <p><i class="fas fa-user"></i> Gender: {{ $data->Gender }}</p>
-                <p><i class="fas fa-birthday-cake"></i> Birthday: {{ date('d/m/Y', strtotime($data->Date_of_Birth)) }}
-                </p>
+{{-- Đống icon nha
+<i class="far fa-user-circle"></i> Username
+<i class="fas fa-address-card"></i> Name
+<i class="fas fa-envelope"></i> Email
+<i class="fas fa-phone"></i> Phone
+<i class="fas fa-home"></i> Address
+<i class="fas fa-user"></i> Gender
+<i class="fas fa-birthday-cake"></i> Birthday --}}
+
+<section style="background-color: #eee;">
+    <div class="container py-5">
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="card mb-4">
+                    <div class="card-body text-center">
+                    @if ($data->Gender == 'male')
+                        <img src="../img/Male.jpg" class="rounded-circle img-fluid" style="width: 273px;">
+                    @elseif ($data->Gender == 'female')
+                        <img src="../img/Female.jpg" class="rounded-circle img-fluid" style="width: 273px;">
+                    @else
+                        <img src="../img/Other.jpg" class="rounded-circle img-fluid" style="width: 273px;">
+                    @endif
+                    <h2 class="my-3">{{ $data->Customer_Name }}</h2>
+                    <h3 class="text-muted mb-1">{{ $data->Customer_Username }}</h3>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-8">
+            <div class="card mb-4">
+                <div class="card-body">
+                <div class="row">
+                    <div class="col-sm-3">
+                        <p class="mb-0">Full Name</p>
+                    </div>
+                    <div class="col-sm-9">
+                        <p class="text-muted mb-0">{{ $data->Customer_Name }}</p>
+                    </div>
+                </div>
+                <hr>
+
+                <div class="row">
+                    <div class="col-sm-3">
+                    <p class="mb-0">Email</p>
+                    </div>
+                    <div class="col-sm-9">
+                    <p class="text-muted mb-0">{{ $data->Email }}</p>
+                    </div>
+                </div>
+                <hr>
+
+                <div class="row">
+                    <div class="col-sm-3">
+                    <p class="mb-0">Phone</p>
+                    </div>
+                    <div class="col-sm-9">
+                    <p class="text-muted mb-0">{{ $data->Phone }}</p>
+                    </div>
+                </div>
+                <hr>
+
+                <div class="row">
+                    <div class="col-sm-3">
+                    <p class="mb-0">Address</p>
+                    </div>
+                    <div class="col-sm-9">
+                    <p class="text-muted mb-0">{{ $data->Address }}</p>
+                    </div>
+                </div>
+                <hr>
+
+                <div class="row">
+                    <div class="col-sm-3">
+                    <p class="mb-0">Gender</p>
+                    </div>
+                    <div class="col-sm-9">
+                    <p class="text-muted mb-0">{{ $data->Gender }}</p>
+                    </div>
+                </div>
+                <hr>
+
+                <div class="row">
+                    <div class="col-sm-3">
+                    <p class="mb-0">Birthday</p>
+                    </div>
+                    <div class="col-sm-9">
+                    <p class="text-muted mb-0">{{ date('d/m/Y', strtotime($data->Date_of_Birth)) }}</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-</div>
+</section>
 
-<!-- Start Contact -->
+
+{{-- <!-- Start Contact -->
 <div class="container py-5">
 </div>
-<!-- End Contact -->
+<!-- End Contact --> --}}
 
 
 @include('Footer')
