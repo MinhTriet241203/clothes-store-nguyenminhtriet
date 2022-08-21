1ded051a928625864b638e130d2bbe0d8e1ec282
MinhTriet241203 <minhtriet241203@gmail.com>
2022-08-21 22:40:20 +0700

    Update profile page

--- resources/views/Customer/profile.blade.php
+++ resources/views/Customer/profile.blade.php
@@ -13,89 +13,96 @@
 <i class="fas fa-user"></i> Gender
 <i class="fas fa-birthday-cake"></i> Birthday --}}
 
-<section style="background-color: #eee;">
-    <div class="container py-5">
-        <div class="row">
-            <div class="col-lg-4">
+@if (Session::has('customerLoginID'))
+    <section style="background-color: #eee;">
+        <div class="container py-5">
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="card mb-4">
+                        <div class="card-body text-center">
+                        @if ($data->Gender == 'male')
+                            <img src="../img/ProfilePics/Placeholder_PFP_Male.jpg" class="rounded-circle img-fluid" style="width: 273px;">
+                        @elseif ($data->Gender == 'female')
+                            <img src="../img/ProfilePics/Placeholder_PFP_Female.jpg" class="rounded-circle img-fluid" style="width: 273px;">
+                        @else
+                            <img src="../img/ProfilePics/Placeholder_PFP_Other.png" class="rounded-circle img-fluid" style="width: 273px;">
+                        @endif
+                        <h2 class="my-3">{{ $data->Customer_Name }}</h2>
+                        <h3 class="text-muted mb-1">{{ $data->Customer_Username }}</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-8">
                 <div class="card mb-4">
-                    <div class="card-body text-center">
-                    @if ($data->Gender == 'male')
-                        <img src="../img/Male.jpg" class="rounded-circle img-fluid" style="width: 273px;">
-                    @elseif ($data->Gender == 'female')
-                        <img src="../img/Female.jpg" class="rounded-circle img-fluid" style="width: 273px;">
-                    @else
-                        <img src="../img/Other.jpg" class="rounded-circle img-fluid" style="width: 273px;">
-                    @endif
-                    <h2 class="my-3">{{ $data->Customer_Name }}</h2>
-                    <h3 class="text-muted mb-1">{{ $data->Customer_Username }}</h3>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-8">
-            <div class="card mb-4">
-                <div class="card-body">
-                <div class="row">
-                    <div class="col-sm-3">
-                        <p class="mb-0">Full Name</p>
+                    <div class="card-body">
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <p class="mb-0">Full Name</p>
+                        </div>
+                        <div class="col-sm-9">
+                            <p class="text-muted mb-0">{{ $data->Customer_Name }}</p>
+                        </div>
                     </div>
-                    <div class="col-sm-9">
-                        <p class="text-muted mb-0">{{ $data->Customer_Name }}</p>
-                    </div>
-                </div>
-                <hr>
+                    <hr>
 
-                <div class="row">
-                    <div class="col-sm-3">
-                    <p class="mb-0">Email</p>
+                    <div class="row">
+                        <div class="col-sm-3">
+                        <p class="mb-0">Email</p>
+                        </div>
+                        <div class="col-sm-9">
+                        <p class="text-muted mb-0">{{ $data->Email }}</p>
+                        </div>
                     </div>
-                    <div class="col-sm-9">
-                    <p class="text-muted mb-0">{{ $data->Email }}</p>
-                    </div>
-                </div>
-                <hr>
+                    <hr>
 
-                <div class="row">
-                    <div class="col-sm-3">
-                    <p class="mb-0">Phone</p>
+                    <div class="row">
+                        <div class="col-sm-3">
+                        <p class="mb-0">Phone</p>
+                        </div>
+                        <div class="col-sm-9">
+                        <p class="text-muted mb-0">{{ $data->Phone }}</p>
+                        </div>
                     </div>
-                    <div class="col-sm-9">
-                    <p class="text-muted mb-0">{{ $data->Phone }}</p>
-                    </div>
-                </div>
-                <hr>
+                    <hr>
 
-                <div class="row">
-                    <div class="col-sm-3">
-                    <p class="mb-0">Address</p>
+                    <div class="row">
+                        <div class="col-sm-3">
+                        <p class="mb-0">Address</p>
+                        </div>
+                        <div class="col-sm-9">
+                        <p class="text-muted mb-0">{{ $data->Address }}</p>
+                        </div>
                     </div>
-                    <div class="col-sm-9">
-                    <p class="text-muted mb-0">{{ $data->Address }}</p>
-                    </div>
-                </div>
-                <hr>
+                    <hr>
 
-                <div class="row">
-                    <div class="col-sm-3">
-                    <p class="mb-0">Gender</p>
+                    <div class="row">
+                        <div class="col-sm-3">
+                        <p class="mb-0">Gender</p>
+                        </div>
+                        <div class="col-sm-9">
+                        <p class="text-muted mb-0">{{ $data->Gender }}</p>
+                        </div>
                     </div>
-                    <div class="col-sm-9">
-                    <p class="text-muted mb-0">{{ $data->Gender }}</p>
-                    </div>
-                </div>
-                <hr>
+                    <hr>
 
-                <div class="row">
-                    <div class="col-sm-3">
-                    <p class="mb-0">Birthday</p>
-                    </div>
-                    <div class="col-sm-9">
-                    <p class="text-muted mb-0">{{ date('d/m/Y', strtotime($data->Date_of_Birth)) }}</p>
+                    <div class="row">
+                        <div class="col-sm-3">
+                        <p class="mb-0">Birthday</p>
+                        </div>
+                        <div class="col-sm-9">
+                        <p class="text-muted mb-0">{{ date('d/m/Y', strtotime($data->Date_of_Birth)) }}</p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
+@else
+    <p>Looks like you have not logged in yet! To see your own profile you need to 
+    <a href="{{ url('customerLogin') }}" style="color: #23B35A">Login here</a> or 
+    <a href="{{ url('customerRegister') }}"style="color: #23B35A">Sign up here</a>.</p>
+@endif
+
 
 
 {{-- <!-- Start Contact -->
