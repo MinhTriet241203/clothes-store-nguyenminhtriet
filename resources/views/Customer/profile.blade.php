b987f6865f7e9de57c1c460ba9d885a8e3c9a37e
mrshine4k <111043449+mrshine4k@users.noreply.github.com>
2022-08-21 21:46:33 +0700

    exception stuffs

--- resources/views/Customer/profile.blade.php
+++ resources/views/Customer/profile.blade.php
@@ -9,20 +9,23 @@
     <div class="col-md-6 m-auto text-center" style="display: inline-block">
         <div class="col-md-5">
             <div class="card rounded-3">
-                @if (Session::has('customerLoginID'))                    
-                <img src="img/ProfilePics/PlaceholderPFP.jpg" alt="" height="250" width="250"
-                    style="border-radius: 25%; align-self:center">
-                <h1>{{ $data->Customer_Name }}</h1>
-                <p><i class="far fa-user-circle"></i> Username: {{ $data->Customer_Username }}</p>
-                <p><i class="fas fa-address-card"></i> Name: {{ $data->Customer_Name }}</p>
-                <p><i class="fas fa-envelope"></i> Email: {{ $data->Email }}</p>
-                <p><i class="fas fa-phone"></i> Phone: {{ $data->Phone }}</p>
-                <p><i class="fas fa-home"></i> Address: {{ $data->Address }}</p>
-                <p><i class="fas fa-user"></i> Gender: {{ $data->Gender }}</p>
-                <p><i class="fas fa-birthday-cake"></i> Birthday: {{ date('d/m/Y', strtotime($data->Date_of_Birth)) }}
-                </p>
+                @if (Session::has('customerLoginID'))
+                    <img src="img/ProfilePics/Placeholder_PFP_Male.jpg" alt="" height="250" width="250"
+                        style="border-radius: 25%; align-self:center">
+                    <h1>{{ $data->Customer_Name }}</h1>
+                    <p><i class="far fa-user-circle"></i> Username: {{ $data->Customer_Username }}</p>
+                    <p><i class="fas fa-address-card"></i> Name: {{ $data->Customer_Name }}</p>
+                    <p><i class="fas fa-envelope"></i> Email: {{ $data->Email }}</p>
+                    <p><i class="fas fa-phone"></i> Phone: {{ $data->Phone }}</p>
+                    <p><i class="fas fa-home"></i> Address: {{ $data->Address }}</p>
+                    <p><i class="fas fa-user"></i> Gender: {{ $data->Gender }}</p>
+                    <p><i class="fas fa-birthday-cake"></i> Birthday:
+                        {{ date('d/m/Y', strtotime($data->Date_of_Birth)) }}
+                    </p>
                 @else
-                <p>Oops! Looks like you have not logged in yet! <a href="">Login here</a> or <a href="">Sign up here</a>.</p>
+                    <p>Looks like you have not logged in yet! To see your own profile you need to <a
+                            href="{{ url('customerLogin') }}" style="color: #23B35A">Login here</a> or <a
+                            href="{{ url('customerRegister') }}"style="color: #23B35A">Sign up here</a>.</p>
                 @endif
             </div>
         </div>
