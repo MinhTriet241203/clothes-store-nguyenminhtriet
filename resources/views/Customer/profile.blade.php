9c40d82dae14f1165530e9d40f091a50aee1f1ff
mrshine4k <111043449+mrshine4k@users.noreply.github.com>
2022-08-21 23:04:36 +0700

    sú

--- resources/views/Customer/profile.blade.php
+++ resources/views/Customer/profile.blade.php
@@ -20,87 +20,92 @@
                 <div class="col-lg-4">
                     <div class="card mb-4">
                         <div class="card-body text-center">
-                        @if ($data->Gender == 'male')
-                            <img src="../img/ProfilePics/Placeholder_PFP_Male.jpg" class="rounded-circle img-fluid" style="width: 273px;">
-                        @elseif ($data->Gender == 'female')
-                            <img src="../img/ProfilePics/Placeholder_PFP_Female.jpg" class="rounded-circle img-fluid" style="width: 273px;">
-                        @else
-                            <img src="../img/ProfilePics/Placeholder_PFP_Other.png" class="rounded-circle img-fluid" style="width: 273px;">
-                        @endif
-                        <h2 class="my-3">{{ $data->Customer_Name }}</h2>
-                        <h3 class="text-muted mb-1">{{ $data->Customer_Username }}</h3>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-8">
-                <div class="card mb-4">
-                    <div class="card-body">
-                    <div class="row">
-                        <div class="col-sm-3">
-                            <p class="mb-0">Full Name</p>
-                        </div>
-                        <div class="col-sm-9">
-                            <p class="text-muted mb-0">{{ $data->Customer_Name }}</p>
-                        </div>
-                    </div>
-                    <hr>
-
-                    <div class="row">
-                        <div class="col-sm-3">
-                        <p class="mb-0">Email</p>
-                        </div>
-                        <div class="col-sm-9">
-                        <p class="text-muted mb-0">{{ $data->Email }}</p>
-                        </div>
-                    </div>
-                    <hr>
-
-                    <div class="row">
-                        <div class="col-sm-3">
-                        <p class="mb-0">Phone</p>
-                        </div>
-                        <div class="col-sm-9">
-                        <p class="text-muted mb-0">{{ $data->Phone }}</p>
-                        </div>
-                    </div>
-                    <hr>
-
-                    <div class="row">
-                        <div class="col-sm-3">
-                        <p class="mb-0">Address</p>
-                        </div>
-                        <div class="col-sm-9">
-                        <p class="text-muted mb-0">{{ $data->Address }}</p>
-                        </div>
-                    </div>
-                    <hr>
-
-                    <div class="row">
-                        <div class="col-sm-3">
-                        <p class="mb-0">Gender</p>
-                        </div>
-                        <div class="col-sm-9">
-                        <p class="text-muted mb-0">{{ $data->Gender }}</p>
-                        </div>
-                    </div>
-                    <hr>
-
-                    <div class="row">
-                        <div class="col-sm-3">
-                        <p class="mb-0">Birthday</p>
-                        </div>
-                        <div class="col-sm-9">
-                        <p class="text-muted mb-0">{{ date('d/m/Y', strtotime($data->Date_of_Birth)) }}</p>
+                            @if ($data->Gender == 'male')
+                                <img src="../img/ProfilePics/Placeholder_PFP_Male.jpg" class="rounded-circle img-fluid"
+                                    style="width: 273px;">
+                            @elseif ($data->Gender == 'female')
+                                <img src="../img/ProfilePics/Placeholder_PFP_Female.jpg" class="rounded-circle img-fluid"
+                                    style="width: 273px;">
+                            @else
+                                <img src="../img/ProfilePics/Placeholder_PFP_Other.png" class="rounded-circle img-fluid"
+                                    style="width: 273px;">
+                            @endif
+                            <h2 class="my-3">{{ $data->Customer_Name }}</h2>
+                            <h3 class="text-muted mb-1">{{ $data->Customer_Username }}</h3>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
+                <div class="col-lg-8">
+                    <div class="card mb-4">
+                        <div class="card-body"  style="font-size:20px;">
+                            <div class="row">
+                                <div class="col-sm-3" style="font-weight: 900 !important;">
+                                    <p class="mb-0"><i class="fas fa-address-card"></i> | Full Name</p>
+                                </div>
+                                <div class="col-sm-9">
+                                    <p class="text-muted mb-0">{{ $data->Customer_Name }}</p>
+                                </div>
+                            </div>
+                            <hr>
+
+                            <div class="row">
+                                <div class="col-sm-3">
+                                    <p class="mb-0"><i class="fas fa-envelope"></i> Email</p>
+                                </div>
+                                <div class="col-sm-9">
+                                    <p class="text-muted mb-0">{{ $data->Email }}</p>
+                                </div>
+                            </div>
+                            <hr>
+
+                            <div class="row">
+                                <div class="col-sm-3">
+                                    <p class="mb-0"><i class="fas fa-phone"></i> Phone</p>
+                                </div>
+                                <div class="col-sm-9">
+                                    <p class="text-muted mb-0">{{ $data->Phone }}</p>
+                                </div>
+                            </div>
+                            <hr>
+
+                            <div class="row">
+                                <div class="col-sm-3">
+                                    <p class="mb-0"><i class="fas fa-home"></i> Address</p>
+                                </div>
+                                <div class="col-sm-9">
+                                    <p class="text-muted mb-0">{{ $data->Address }}</p>
+                                </div>
+                            </div>
+                            <hr>
+
+                            <div class="row">
+                                <div class="col-sm-3">
+                                    <p class="mb-0"><i class="fas fa-user"></i> Gender</p>
+                                </div>
+                                <div class="col-sm-9">
+                                    <p class="text-muted mb-0">{{ $data->Gender }}</p>
+                                </div>
+                            </div>
+                            <hr>
+
+                            <div class="row">
+                                <div class="col-sm-3">
+                                    <p class="mb-0"><i class="fas fa-birthday-cake"></i> Birthday</p>
+                                </div>
+                                <div class="col-sm-9">
+                                    <p class="text-muted mb-0">{{ date('d/m/Y', strtotime($data->Date_of_Birth)) }}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <hr>
     </section>
 @else
-    <p>Looks like you have not logged in yet! To see your own profile you need to 
-    <a href="{{ url('customerLogin') }}" style="color: #23B35A">Login here</a> or 
-    <a href="{{ url('customerRegister') }}"style="color: #23B35A">Sign up here</a>.</p>
+    <p>Looks like you have not logged in yet! To see your own profile you need to
+        <a href="{{ url('customerLogin') }}" style="color: #23B35A">Login here</a> or
+        <a href="{{ url('customerRegister') }}"style="color: #23B35A">Sign up here</a>.
+    </p>
 @endif
 
 
